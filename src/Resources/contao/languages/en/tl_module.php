c7a732d3ca4a3a7a92f3fff3ce8c7e4e3439cb07
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 11:07:15 -0400

    Update tl_module.php

--- src/Resources/contao/languages/en/tl_module.php
+++ src/Resources/contao/languages/en/tl_module.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Member Auto Activate
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-member-auto-activate
+ */
+
 /** Fields */
 $GLOBALS['TL_LANG']['tl_module']['reg_autoActivate'] 		= array('Auto activate', 'Auto activate member upon registration.');
 $GLOBALS['TL_LANG']['tl_module']['reg_autoActivateDomains'] = array('Auto activate domains', 'Limit auto activation to emails from these domains. One domain per line. Leave blank to auto activate all.');
