66f3d9820b285fe7f9c5278090afc89e106792d8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 11:07:40 -0400

    Update MemberHooks.php

--- src/Resources/contao/library/Bcs/Hooks/MemberHooks.php
+++ src/Resources/contao/library/Bcs/Hooks/MemberHooks.php
@@ -1,5 +1,13 @@
 <?php
 
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Member Auto Activate
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-member-auto-activate
+ */
+
 namespace Bcs\Hooks;
 
 use Contao\Frontend;
