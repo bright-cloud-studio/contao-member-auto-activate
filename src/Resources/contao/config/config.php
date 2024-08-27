8cc01adf19d8ad2903de7b4643b9f329b62d8c6c
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 11:06:46 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
-* @copyright  Bright Cliud Studio
-* @author     Bright Cloud Studio
-* @package    Contao Member Auto Activate
-* @license    LGPL-3.0+
-* @see	       https://github.com/bright-cloud-studio/contao-member-auto-activate
-*/
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Member Auto Activate
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-member-auto-activate
+ */
 
 /* Hooks */
 $GLOBALS['TL_HOOKS']['createNewUser'][]        = array('Bcs\Hooks\MemberHooks', 'onCreateNewUser');
