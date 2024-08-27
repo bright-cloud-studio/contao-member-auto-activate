893f39fb45d22439e1941f8744ca814113d64a27
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 10:22:46 -0400

    Create tl_module.php

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,25 @@
+<?php
+
+/** Palettes */
+$GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][] = 'reg_autoActivate';
+$GLOBALS['TL_DCA']['tl_module']['palettes']['registration']	= str_replace('reg_allowLogin', 'reg_allowLogin,reg_autoActivate', $GLOBALS['TL_DCA']['tl_module']['palettes']['registration']);
+$GLOBALS['TL_DCA']['tl_module']['subpalettes']['reg_autoActivate'] = 'reg_autoActivateDomains';
+
+
+/** Fields */
+$GLOBALS['TL_DCA']['tl_module']['fields']['reg_autoActivate'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['reg_autoActivate'],
+	'exclude'                 => true,
+	'inputType'               => 'checkbox',
+	'eval'                    => array('submitOnChange'=>true),
+	'sql'                     => "char(1) NOT NULL default ''"
+);
+
+$GLOBALS['TL_DCA']['tl_module']['fields']['reg_autoActivateDomains'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_module']['reg_autoActivateDomains'],
+	'inputType'               => 'textarea',
+	'eval'                    => array('tl_class'=>'clr'),
+	'sql'                     => "text NULL"
+);
