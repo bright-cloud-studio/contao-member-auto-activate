1445ca3ef1658cd8816dc7c7954d52058ce2b75a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 11:08:05 -0400

    Update tl_module.php

--- src/Resources/contao/tl_module.php
+++ src/Resources/contao/tl_module.php
@@ -1,15 +1,19 @@
 <?php
 
 /**
- * Palettes
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Member Auto Activate
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-member-auto-activate
  */
+
+/** Palettes */
 $GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][] = 'reg_autoActivate';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['registration']	= str_replace('reg_allowLogin', 'reg_allowLogin,reg_autoActivate', $GLOBALS['TL_DCA']['tl_module']['palettes']['registration']);
 $GLOBALS['TL_DCA']['tl_module']['subpalettes']['reg_autoActivate'] = 'reg_autoActivateDomains';
 
-/**
- * Fields
- */
+/** Fields */
 $GLOBALS['TL_DCA']['tl_module']['fields']['reg_autoActivate'] = array
 (
   'label'                   => &$GLOBALS['TL_LANG']['tl_module']['reg_autoActivate'],
