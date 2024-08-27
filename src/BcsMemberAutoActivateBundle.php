45aaa18fcdb3cfbfd8f3ea7c93d32de5b8952111
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 08:55:42 -0400

    Create BcsMemberAutoActivateBundle.php

---
+++ src/BcsMemberAutoActivateBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cliud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao Member Auto Activate
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-member-auto-activate
+ */
+
+namespace Bcs\PBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsMemberAutoActivateBundle extends Bundle
+{
+}
