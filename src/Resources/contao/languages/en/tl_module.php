2434042c1daeb0ac9832ba78167edf05de04d683
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-08-27 10:19:07 -0400

    Create tl_module.php

---
+++ src/Resources/contao/languages/en/tl_module.php
@@ -0,0 +1,5 @@
+<?php
+
+/** Fields */
+$GLOBALS['TL_LANG']['tl_module']['reg_autoActivate'] 		= array('Auto activate', 'Auto activate member upon registration.');
+$GLOBALS['TL_LANG']['tl_module']['reg_autoActivateDomains'] = array('Auto activate domains', 'Limit auto activation to emails from these domains. One domain per line. Leave blank to auto activate all.');
